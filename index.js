e303801e7c54b80950eb1fe026b68794e8a4a8cd
sailaxmi1515 <76105041+sailaxmi1515@users.noreply.github.com>
2022-11-16 23:28:40 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = request("http");
-const fs = require("fs");
-const server = http.createServer((req,res) => {
-    fs.readFile("sample.txt",(err,data) => {
-        res.end(data);
-    });
-});
-server.listen(3000);
